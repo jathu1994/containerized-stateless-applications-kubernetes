07106a7b0fb07ea747a0b986ae1947e6308c65b6
jatharthan <jathu10061994@gmail.com>
2023-03-10 17:04:19 +0000

    adding force delete to resolve hanging in tf destroy

--- ec2_kind/ecr.tf
+++ ec2_kind/ecr.tf
@@ -1,8 +1,10 @@
 #creating ecr
 resource "aws_ecr_repository" "webappassignment1" {
-  name = "webapp_assignment1"
+  name = "webapp"
+  force_delete = true
 }
 
 resource "aws_ecr_repository" "mysqlassignment1" {
-  name = "mysql_assignment1"
+  name = "mysql_db"
+  force_delete = true
 }
